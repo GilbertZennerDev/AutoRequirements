728cc9da6d89535060b66976ad8a64dbb0ea1673
gzenner <gzenner@student.42luxembourg.lu>
2025-10-27 14:24:32 +0100

    better

--- importer.py
+++ importer.py
@@ -1,27 +1,34 @@
+"""
+I need to handle these cases:
+import bla (handled)
+from bla import * (handled)
+from bla import * as bla2 (just cut out anything from 'as')
+"""
+
 import sys
 import subprocess as sp
 
 def loopfiles(imports=[]):
 	filenames = [sys.argv[i] for i in range(2, len(sys.argv))]
 	for filename in filenames:
-		imports += [line + ' ' for line in open(filename, 'r').read().splitlines() if 'import' in line]
-	return imports
+		content = open(filename, 'r').read().splitlines()
+		content = [line for line in content if 'import' in line]
+		content = [line[:line.find('as')] if 'as' in line else line for line in content]
+		#imports += " ".join(content)
+	return " ".join(content)
 
 def fiximports():
 	if len(sys.argv) < 2:
-		print('Argument show or install required')
+		print('Argument "show" or "install" required')
 		exit()
 	mode = sys.argv[1]
 	imports = loopfiles()
 	imports = [line.replace(',', '') for line in ''.join(imp for imp in imports).split()]
-	imports = [line for line in imports if line != 'import' and line != 'from']
+	imports = [line.strip() for line in imports if line != 'import' and line != 'from' and line != '*']
 	imports = set(imports)
-	open("requirements.txt", "w").write("\n".join(imports))
-	if mode == 'show':
-		print(imports)
-	elif mode == 'install':
-		sp.run(['pip', 'install', *imports])
-	print("Fixed the Imports. Created requirements.txt")
+	savedcontent = "\n".join(imports)
+	open("requirements.txt", "w").write(savedcontent)
+	print("Created requirements.txt for:\n", savedcontent)
 	
 fiximports()
 
---
+++ test.py
@@ -0,0 +1,3 @@
+import requests as r
+import mod1, mod2, mod3
+from bla import * as name
